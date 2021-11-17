aa4c5cd4a55f56e0f2d1cb409fe7de7012e4b3c0
Paras Kamble <paraskamble8268@gmail.com>
2021-11-18 00:48:43 +0530

    Added comments

--- library_api/views.py
+++ library_api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from . models import Book
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
-import cloudinary.uploader 
+import cloudinary.uploader
 from django.conf import settings
 from .forms import BookForm
 # cloudinary.config(
@@ -69,10 +69,10 @@ def all_books(request):
     data = Book.objects.all().order_by('-id')
     return render(request, 'all_books.html', {'data': data})
 
+# To retrieve particular books
 def author_books(request):
     data = Book.objects.filter(user=request.user).order_by('-id')
     return render(request, 'all_books.html', {'data': data})
-
 
 # To create new records of the books
 @login_required(login_url='login')
@@ -80,11 +80,12 @@ def create_book_record(request):
     if request.method == 'POST':
         try:
             user = request.user
-            context = dict( backend_form = BookForm())
+            context = dict(backend_form=BookForm())
             form = BookForm(request.POST, request.FILES)
             context['posted'] = form.instance
             if form.is_valid():
-                data = Book(books_name = form.cleaned_data['books_name'], books_img = form.cleaned_data['books_img'], books_description= form.cleaned_data['books_description'], user=user)
+                data = Book(books_name=form.cleaned_data['books_name'], books_img=form.cleaned_data['books_img'],
+                            books_description=form.cleaned_data['books_description'], user=user)
                 data.save()
             messages.info(request, 'New Book record created')
         except Exception as e:
@@ -92,6 +93,7 @@ def create_book_record(request):
             return redirect('create_book_record')
     return render(request, 'create_book_record.html')
 
+# To update a record in the database
 def update_book_record(request, pk):
     books_data = Book.objects.get(id=pk)
     if books_data.user != request.user:
@@ -100,22 +102,25 @@ def update_book_record(request, pk):
         try:
             form = BookForm(request.POST, request.FILES, instance=books_data)
             if form.is_valid():
-                books_data.books_name = form.cleaned_data['books_name'] if form.cleaned_data.get('books_name') else books_data.books_name
-                books_data.books_img = form.cleaned_data['books_img'] if form.cleaned_data.get('books_img') else books_data.books_img
-                books_data.books_description = form.cleaned_data['books_description'] if form.cleaned_data.get('books_description') else books_data.books_description
+                books_data.books_name = form.cleaned_data['books_name'] if form.cleaned_data.get(
+                    'books_name') else books_data.books_name
+                books_data.books_img = form.cleaned_data['books_img'] if form.cleaned_data.get(
+                    'books_img') else books_data.books_img
+                books_data.books_description = form.cleaned_data['books_description'] if form.cleaned_data.get(
+                    'books_description') else books_data.books_description
                 books_data.save()
             messages.info(request, 'Book record updated')
         except Exception as e:
             messages.error(request, e)
             return redirect('create_book_record')
 
-    
     return render(request, 'update_book_record.html', {'books_data': books_data})
 
+# To remove the record from the database.
 def delete_record(request, pk):
     books_data = Book.objects.get(id=pk)
     if books_data.user != request.user:
         return redirect('home')
-    books_data.delete()   
+    books_data.delete()
+    messages.info(request, 'Book record deleted')
     return redirect('all_books')
-
